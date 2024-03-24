11d86d2abce23b43d3b0c664b22f16778c7f5d25
JianYuChentw <coke818572@gmail.com>
2024-03-24 12:53:33 +0800

    update:部分資訊加入環境變數

--- database/data.js
+++ database/data.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 // 資料庫連線資料
 const connection = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'pass123456',
-  database: 'my_to_do_list',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 // 測試資料庫連線
